0c688e09d4866a790e39e72ea4a355f87481e52a
ka7788158-png <ka7788158@gmail.com>
2026-09-17 23:55:29 +0530

    making small correction

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from backend import run_travel_agent
 BASE_DIR = Path(__file__).resolve().parent
 
 app = FastAPI(
-    title="TripMate AI",
+    title="JourneyHacker AI",
     description="LangGraph Multi-Agent Travel Planner with FastAPI Frontend",
     version="1.0.0"
 )
